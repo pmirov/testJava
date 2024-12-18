f596ef679e48bcf6cc4d2129f60c45b17f4d8fda
pmirov <134385584+pmirov@users.noreply.github.com>
2024-12-18 21:54:03 +0700

    Add files via upload

---
+++ src/testClass.java
@@ -0,0 +1,11 @@
+public class testClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //текст
+        //новый коммент
+
+        System.out.println("Hello pops!");
+
+    }
+}
